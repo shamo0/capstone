0cea761cd26342e57be82553e1d1550abcffeef1
Geno Shamugia <48299520+shamo0@users.noreply.github.com>
2020-09-10 09:15:15 -0700

    now can process multiple csv files at a time

--- script.py
+++ script.py
@@ -1,22 +1,39 @@
+#!/usr/bin/python3
+'''
+Author: Shamugia Genadi
+Project: User Frequency Calculator
+Description: Program takes csv files which are twitter scraper captures and counts the number of occurances of each username. 
+'''
+#Importing used librarires
 import csv
 import sys
 import ast
+import os
+import glob
 
 def main():
-    usernameList = []
 
-    with open(sys.argv[1]) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            try:
-                cell = ast.literal_eval(row[4])
-                username = cell['user_mentions'][0]['screen_name']
-                usernameList.append(username)
-                line_count += 1
-            except Exception:
-                line_count +=1
-                pass
+    path = sys.argv[1]
+    files = os.listdir(path)
+
+    # files_csv = [i for i in files if i.endswith('.csv')]
+    usernameList = [] #List for all the usernames
+    #Open and read the csv file.
+    for f in files:
+        if f.endswith('.csv'):
+            print('Reading file ' + f + '. Please wait...')
+            with open(path+'/'+str(f)) as csv_file: 
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                line_count = 0
+                for row in csv_reader:
+                    try:
+                        cell = ast.literal_eval(row[4])
+                        username = cell['user_mentions'][0]['screen_name'] #grab the username
+                        usernameList.append(username)
+                        line_count += 1
+                    except Exception: #Exception for unusual cases
+                        line_count +=1
+                        pass
     
     CountFrequency(usernameList) 
 
@@ -28,9 +45,15 @@ def CountFrequency(my_list):
             freq[item] += 1
         else: 
             freq[item] = 1
-    
-  
-    for key, value in freq.items(): 
-        print ("% s : % d"%(key, value)) 
 
+    #order the Dictionary by Value in descending order
+    sorted_orders = sorted(freq.items(), key=lambda x:x[1],reverse=True)
+    # print(type(sorted_orders))
+
+
+    #Write to a csv file one row at a time 
+    with open("output.csv", "a") as fp:
+        wr = csv.writer(fp, dialect='excel')
+        wr.writerows(sorted_orders)
+    fp.close()
 main()
